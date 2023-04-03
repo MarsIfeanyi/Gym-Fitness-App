1aae226d672052101514ee2b014a777751f3827d
MarsIfeanyi <marschalice@gmail.com>
2023-04-03 01:29:03 +0100

    Add Styles and render Home and Navbar Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import { useState } from "react";
+
+import Navbar from "@/scenes/navbar";
+
+enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUS = "contactus",
+}
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      //checks if we are at the top of the page and the sets "setIsTopOfPage" to true.
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Fragment>
-      <div>App</div>
+      <div className="bg-gray-20">
+        <Navbar
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+          isTopOfPage={isTopOfPage}
+        />
+
+        <Home />
+      </div>
     </Fragment>
   );
 }
