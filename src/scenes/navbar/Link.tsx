cec1a9ef4c2619ce66ef085de6011b5d407a1037
MarsIfeanyi <marschalice@gmail.com>
2023-04-03 01:38:10 +0100

    Create Page link component

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,31 @@
+import { SelectedPage } from "@/shared/type";
+import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void; // Hint: setSelectedPage is a function
+};
+
+// Hint: The useState() for selectedPage page is defined in the App.tsx.
+
+// VIP: The Link component is used in the Navbar component(index.tsx)
+
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
+  return (
+    // Hint: selectedPage represents a state
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+        transition duration-500 hover:text-primary-300
+      `}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+export default Link;
