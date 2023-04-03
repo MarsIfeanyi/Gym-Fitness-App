b653369379dc4ca619a0d451893d20eb88466b87
MarsIfeanyi <marschalice@gmail.com>
2023-04-03 21:38:42 +0100

    Add custom types

--- src/shared/type.ts
+++ src/shared/type.ts
@@ -4,3 +4,16 @@ export enum SelectedPage {
   OurClasses = "ourclasses",
   ContactUS = "contactus",
 }
+
+export interface BenefitType {
+  icon: JSX.Element;
+  id: number;
+  title: string;
+  description: string;
+}
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
