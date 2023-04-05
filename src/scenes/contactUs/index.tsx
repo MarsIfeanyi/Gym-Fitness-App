85173f81c906662abecf09e4fa5da95b35b050dd
MarsIfeanyi <marschalice@gmail.com>
2023-04-05 20:18:17 +0100

    Change ContactUS to ContactUs

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -29,7 +29,7 @@ const ContactUS = ({ setSelectedPage }: Props) => {
   return (
     <section id="contactus" className="mx-auto w-5/6 pb-32 pt-24">
       <motion.div
-        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUS)}
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
       >
         {/* HEADER */}
         <motion.div
