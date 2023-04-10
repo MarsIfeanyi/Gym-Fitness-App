ed4e9abd840aa0fae01f6dd1fa5448cc4cd2816e
MarsIfeanyi <marschalice@gmail.com>
2023-04-10 09:06:33 +0100

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,43 @@
-import { Fragment, useEffect } from "react";
-import { useState } from "react";
-
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
-import Benefits from "./scenes/benefits";
-import OurClasses from "./scenes/ourClasses";
-import ContactUS from "./scenes/contactUs";
-import Footer from "./scenes/footer";
-
-enum SelectedPage {
-  Home = "home",
-  Benefits = "benefits",
-  OurClasses = "ourclasses",
-  ContactUS = "contactus",
-}
+import OurClasses from "@/scenes/ourClasses";
+import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
-
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
   useEffect(() => {
     const handleScroll = () => {
-      //checks if we are at the top of the page and the sets "setIsTopOfPage" to true.
       if (window.scrollY === 0) {
         setIsTopOfPage(true);
         setSelectedPage(SelectedPage.Home);
       }
-      window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
     };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <Fragment>
-      <div className="bg-gray-20">
-        <Navbar
-          selectedPage={selectedPage}
-          setSelectedPage={setSelectedPage}
-          isTopOfPage={isTopOfPage}
-        />
-
-        <Home setSelectedPage={setSelectedPage} />
-
-        <Benefits setSelectedPage={setSelectedPage} />
-
-        <OurClasses setSelectedPage={setSelectedPage} />
-
-        <ContactUS setSelectedPage={setSelectedPage} />
-
-        <Footer />
-      </div>
-    </Fragment>
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
+    </div>
   );
 }
 
