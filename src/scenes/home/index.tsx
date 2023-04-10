c3578246533883144830957b13314db1e92aee90
MarsIfeanyi <marschalice@gmail.com>
2023-04-10 09:08:10 +0100

    fix bug

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,6 +1,6 @@
 import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
-import { SelectedPage } from "@/shared/type";
+import { SelectedPage } from "@/shared/types";
 import HomePageText from "@/assets/HomePageText.png";
 import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
